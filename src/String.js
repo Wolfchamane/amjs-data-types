6b15eede2c2235491d874c5b48d6554b7d7f5dc8
Wolfchamane <arturo.mart.diaz@gmail.com>
2019-06-06 18:29:08 +0200

    feature (*): String type

---
+++ src/String.js
@@ -0,0 +1,26 @@
+const AmjsDataTypesBase = require('./Base');
+
+/**
+ * Type string
+ * @namespace   amjs.dataTypes.type
+ * @class       amjs.dataTypes.type.String
+ * @extends     amjs.dataTypes.type.Base
+ */
+class AmjsDataTypesTypeString extends AmjsDataTypesBase
+{
+    /**
+     * @override
+     */
+    _parseValue(value)
+    {
+        if (typeof value !== 'string')
+        {
+            value = String(value);
+        }
+
+        return super._parseValue(value);
+    }
+}
+
+AmjsDataTypesBase.register('String', AmjsDataTypesTypeString);
+module.exports = AmjsDataTypesTypeString;
---
+++ tests/unit/String.js
@@ -0,0 +1,48 @@
+const AmjsDataTypesString   = require('../../src/String');
+const AmjsFactory           = require('@amjs/factory');
+const assert                = require('assert');
+
+/**
+ * Asserts that class is registered as "String"
+ */
+function testRegistrationName()
+{
+    const sut = AmjsFactory.create('String');
+    assert.equal(
+        sut instanceof AmjsDataTypesString,
+        true,
+        'Is registered as "String"'
+    );
+}
+
+/**
+ * Asserts the override private method "_parseValue"
+ */
+function testParseValue()
+{
+    const values = [
+        null,
+        undefined,
+        {},
+        [],
+        true,
+        false,
+        1,
+        0,
+        NaN,
+        'foo'
+    ];
+
+    values.forEach(
+        value =>
+        {
+            const sut = AmjsFactory.create('String');
+            const result = String(value);
+            sut.value = value;
+            assert.equal(sut.value, result, `String value of "${value}" is: ${result}`);
+        }
+    );
+}
+
+testRegistrationName();
+testParseValue();
