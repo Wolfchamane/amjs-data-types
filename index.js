21541c884df9adc57e544ffd79dc19451100b948
Wolfchamane <arturo.mart.diaz@gmail.com>
2019-06-06 18:29:33 +0200

    feature (index): created index file

---
+++ index.js
@@ -0,0 +1,15 @@
+const AmjsDataTypesBase     = require('./src/Base');
+const AmjsDataTypesBoolean  = require('./src/Boolean');
+const AmjsDataTypesString   = require('./src/String');
+
+/**
+ * @author  Arturo Martínez Díaz <arturo.mart.diaz@gmail.com>
+ * @license MIT
+ * @version 0.1.0
+ * @created 2019-XX-XXTXX:XX:XX.XXX
+ */
+module.exports = {
+    Base    : AmjsDataTypesBase,
+    Boolean : AmjsDataTypesBoolean,
+    String  : AmjsDataTypesString
+};
