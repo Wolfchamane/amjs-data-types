0cbb0e3d2d93424671702cd25785c085e25cc377
Wolfchamane <arturo.mart.diaz@gmail.com>
2019-07-10 09:41:09 +0200

    fix (index.js): moved from 'build/' to '/'

---
+++ index.js
@@ -0,0 +1,28 @@
+const AmjsDataTypesArray = require('./src/Array.js');
+const AmjsDataTypesBase = require('./src/Base.js');
+const AmjsDataTypesBoolean = require('./src/Boolean.js');
+const AmjsDataTypesCollection = require('./src/Collection.js');
+const AmjsDataTypesDate = require('./src/Date.js');
+const AmjsDataTypesNumber = require('./src/Number.js');
+const AmjsDataTypesObject = require('./src/Object.js');
+const AmjsDataTypesPassword = require('./src/Password.js');
+const AmjsDataTypesString = require('./src/String.js');
+
+/**
+ * @NOTICE: File auto-generated. DO NOT modify this file.
+ * @author  Arturo Martínez Díaz &lt;arturo.mart.diaz@gmail.com&gt;
+ * @version 0.1.1
+ * @license MIT
+ * @created 2019-07-10T07:23:56.892Z
+ */
+module.exports = {
+    AmjsDataTypesArray: AmjsDataTypesArray,
+    AmjsDataTypesBase: AmjsDataTypesBase,
+    AmjsDataTypesBoolean: AmjsDataTypesBoolean,
+    AmjsDataTypesCollection: AmjsDataTypesCollection,
+    AmjsDataTypesDate: AmjsDataTypesDate,
+    AmjsDataTypesNumber: AmjsDataTypesNumber,
+    AmjsDataTypesObject: AmjsDataTypesObject,
+    AmjsDataTypesPassword: AmjsDataTypesPassword,
+    AmjsDataTypesString: AmjsDataTypesString
+};
