559349c640ffbdfdeb6adaf7424148bfe9e7c3b6
Wolfchamane <arturo.mart.diaz@gmail.com>
2020-11-20 07:57:02 +0100

    0.1.5 -> 0.1.6

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const AmjsDataTypesBase = require('./src/Base.js');
 const AmjsDataTypesBoolean = require('./src/Boolean.js');
 const AmjsDataTypesCollection = require('./src/Collection.js');
 const AmjsDataTypesDate = require('./src/Date.js');
+const AmjsDataTypesMoney = require('./src/Money.js');
 const AmjsDataTypesNumber = require('./src/Number.js');
 const AmjsDataTypesObject = require('./src/Object.js');
 const AmjsDataTypesPassword = require('./src/Password.js');
@@ -11,9 +12,9 @@ const AmjsDataTypesString = require('./src/String.js');
 /**
  * @NOTICE: File auto-generated. DO NOT modify this file.
  * @author  Arturo Martínez Díaz &lt;arturo.mart.diaz@gmail.com&gt;
- * @version 0.1.1
+ * @version 0.1.6
  * @license MIT
- * @created 2019-07-10T07:23:56.892Z
+ * @created 2020-11-20T06:53:48.830Z
  */
 module.exports = {
     AmjsDataTypesArray: AmjsDataTypesArray,
@@ -21,6 +22,7 @@ module.exports = {
     AmjsDataTypesBoolean: AmjsDataTypesBoolean,
     AmjsDataTypesCollection: AmjsDataTypesCollection,
     AmjsDataTypesDate: AmjsDataTypesDate,
+    AmjsDataTypesMoney: AmjsDataTypesMoney,
     AmjsDataTypesNumber: AmjsDataTypesNumber,
     AmjsDataTypesObject: AmjsDataTypesObject,
     AmjsDataTypesPassword: AmjsDataTypesPassword,
